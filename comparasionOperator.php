3d1d9a1d3c6eb092762a3beffe883eb0304078ff
adirahman <kodingku@github.com>
2018-10-13 21:49:44 +0700

    update comparasion

---
+++ comparasionOperator.php
@@ -0,0 +1,20 @@
+<?php 
+
+/**
+ * Comparasion Operator
+ * Operator komparasi adalah sebuah operator perbandingan yang digunakan untuk membandingkan 2 nilai
+ * Contoh Penggunaan :
+ * NOTE : untuk penggunaan operator lebih lanjut dapat mengunjungi situs : 
+ * https://www.w3schools.com/php/php_operators.asp
+ */
+
+ $a = 10;
+ $b = 10;
+ var_dump($a==$b);//hasil true karna variabel a dan b memiliki nilai yang sama 
+
+
+
+
+
+
+?>
