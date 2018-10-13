528543cb6ce85971d82627dd2a5c7f779e4c0806
adirahman <kodingku@github.com>
2018-10-13 21:43:21 +0700

    operator assignment

---
+++ assignmentOperator.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Operator Penugasan
+ * Operator penugasan dapat disebut juga memasukan nilai ke dalam variabel
+ * Berikut Contohnya
+ */
+
+ $x = 10;
+ $x += 100;
+ echo $x;   
+
+
+
+?>
