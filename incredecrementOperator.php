88e11635464bbb917da58936ffd6f4791e3d141b
adirahman <kodingku@github.com>
2018-10-13 21:57:22 +0700

    upload increment

---
+++ incredecrementOperator.php
@@ -0,0 +1,25 @@
+<?php 
+
+/**
+ * Increment / Decrement Operator
+ * Operator Increment digunakan untuk menaikan nilai variabel
+ * Operator Decrement digunakan untuk menurunkan nilai variabel
+ * 
+ */
+
+ $x = 10;
+ echo $x++;//nilai tetap
+ echo '<br>';
+ $y = 10;
+ echo ++$y;//nilai naik 1 ditambah 10 maka akan menghasilkan 11
+ echo '<br>';
+
+ $z = 20;
+ echo $z++;//nilai tetap
+ echo '<br>';
+ $w = 20;
+ echo --$w;//nilai turun 1 dikurang 20 maka akan menghasilkan 19
+
+
+
+?>
