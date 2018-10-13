ec79e7855aca5dee40326c89c6235782b511a24f
adirahman <kodingku@github.com>
2018-10-13 21:29:57 +0700

    update arithmatic

---
+++ arithmaticOperator.php
@@ -0,0 +1,30 @@
+<?php 
+/**
+ * Operator PHP
+ * Operator pada PHP digunakan untuk melakukan operasi nilai dalam varibel
+ * Ada beberapa operator dalam PHP
+ * - Arithmaitic Operator digunakan untuk melakukan penjumlahan,pengurangan,pembagian,perkalian angka
+ * 
+ */
+
+ // operator pertambahan
+ $one = 1;
+ $two = 2;
+ echo  $one + $two; 
+ echo '<br>';
+ // operator pertambahan
+ $three = 4 ;
+ $four = 2;
+ echo  $three - $four;
+ echo '<br>';
+ // operator pembagian
+ $five = 20;
+ $six = 2;
+ echo  $five / $six;
+ echo '<br>';
+ // operator perkalian
+ $seven = 10;
+ $eight = 2;
+ echo  $seven * $eight;
+ echo '<br>';
+?>
