4ffdb4419fda975a5ae48bb7a4f6e273f30d1a1c
adirahman <kodingku@github.com>
2018-10-13 20:38:20 +0700

    upload variabel

---
+++ variabel.php
@@ -0,0 +1,37 @@
+
+<?php
+    /*
+     Variabel 
+     Untuk membuat variabel atau mendeklarasikan variabel dalam PHP
+     kita dapat menuliskan dengan tanda $
+
+    contoh : 
+    - Pada Contoh dibawah ini kita akan mendeklarasikan variabel nama dengan isi 'lorem'
+    - untuk mencetak tulisan atau nilai dalam variabel pada PHP kita dapat menggunakan perintah echo
+      implementasi : 
+      - $nama = 'lorem' dapat dicetak dengan echo $nama
+    - Dalam Penulisan PHP jangan lupa untuk menutup sintaks dengan menggunakan titik koma(;)
+    - silahkan ketik kode dibawah ini dan jalankan pada browser anda
+    */
+
+    /**
+     * implementasi 1
+     */
+    $nama = 'lorem';
+    echo $nama;
+    /**
+     * impelmentasi 2
+     * pada implementasi kali ini kita akan menggabungkan variabel dengan teks
+     * untuk menggabungkan variabel / nilai dalam PHP dapat menggunakan titik(.) atau disebut dengan concatetination
+     * untuk memberikan jarak atau enter dalam PHP dapat mengguanakn tag HTML yaitu <br>
+     */
+    echo '<br>';
+    $nama2 = 'Nama Saya ' . $nama;
+    echo $nama2
+
+
+
+
+    
+
+?>
