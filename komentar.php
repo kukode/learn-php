ecbb128d231caf889fc7c525e52641fddb8d510c
adirahman <kodingku@github.com>
2018-10-13 21:36:03 +0700

    upload komentar

---
+++ komentar.php
@@ -0,0 +1,19 @@
+<?php 
+
+/**
+ * Komentar pada PHP
+ * untuk komentar pada PHP terdapat 2 bagian yaitu
+ * - multiline komentar
+ * - single komentar
+ * Untuk implemenasi dapat dilihat contoh di bawah ini
+ * Note : Komentar tidak akan di tampilakan pada browser,komentar sangat berfungsi bagi developer agar bisa tahu fungsi dan pekerjaan yang sudah dilakukan
+ */
+
+ /**
+  * Multi Line Komentar
+  */
+
+// Single Line Komentar
+
+
+?>
